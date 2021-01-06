09d224f2da923042b135c68af5944a576aa053ac
Daniel <danieleduardocald@gmail.com>
2021-01-06 10:37:30 -0500

    six

--- db/migrate/20210105022339_create_players_table.rb
+++ db/migrate/20210105022339_create_players_table.rb
@@ -2,35 +2,35 @@ class CreatePlayersTable < ActiveRecord::Migration[5.2]
   def change
     create_table :players do |t|
       t.string :name
-      # t.integer :age
+      t.integer :age
       t.string :nationality
-      # t.integer :height
-      # t.integer :weight
+      t.integer :height
+      t.integer :weight
       t.integer :club_id
-      # t.integer :appearances 
-      # t.integer :minutes
-      # t.string :position
-      # t.float :rating
-      # t.integer :shots
-      # t.integer :shots_on_target
-      # t.integer :goals
-      # t.integer :assists
-      # t.integer :tackles
-      # t.integer :blocks
-      # t.integer :interceptions
-      # t.integer :duels
-      # t.integer :duels_won
-      # t.integer :dribbles_attempted
-      # t.integer :dribbles_successful
-      # t.integer :fouls_drawn
-      # t.integer :fouls_committed
-      # t.integer :yellow_cards
-      # t.integer :red_cards
-      # t.integer :penalties_won
-      # t.integer :penalties_committed
-      # t.integer :penalties_scored
-      # t.integer :penalties_missed
-      # t.integer :penalties_saved
+      t.integer :appearances 
+      t.integer :minutes
+      t.string :position
+      t.float :rating
+      t.integer :shots
+      t.integer :shots_on_target
+      t.integer :goals
+      t.integer :assists
+      t.integer :tackles
+      t.integer :blocks
+      t.integer :interceptions
+      t.integer :duels
+      t.integer :duels_won
+      t.integer :dribbles_attempted
+      t.integer :dribbles_successful
+      t.integer :fouls_drawn
+      t.integer :fouls_committed
+      t.integer :yellow_cards
+      t.integer :red_cards
+      t.integer :penalties_won
+      t.integer :penalties_committed
+      t.integer :penalties_scored
+      t.integer :penalties_missed
+      t.integer :penalties_saved
     end
   end
 end
